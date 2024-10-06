c016e54e248ff7dcad0e1881c05d6e06612ef114
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2024-10-06 21:15:45 +0900

    #1 chore on air layout : layout fixed (scrollbar, card shadow, kakao login)
    * #1-chore-onAirLayout : layout fixed (scrollbar, card shadow, kakao login)

--- app/(main)/_components/tab-bar/tab-bar.tsx
+++ app/(main)/_components/tab-bar/tab-bar.tsx
@@ -11,32 +11,32 @@ import RecommIcon from "@/public/icons/GNB/recomm.svg";
 import RecommIconAbled from "@/public/icons/GNB/recomm_abled.svg";
 
 export default function TapBar() {
-  return (
-    <div className="flex flex-row justify-evenly items-center shadow-md z-20 fixed bottom-0 pb-6 w-full max-w-[450px] bg-white pt-1">
-      <TabBarButton
-        title="home"
-        iconSelected={<HomeIconAbled className="h-6 w-6" />}
-        iconNonSelected={<HomeIcon className="h-6 w-6" />}
-        displayName="홈"
-      />
-      <TabBarButton
-        title="onAir"
-        iconSelected={<OnAirIconAbled className="h-6 w-6 " />}
-        iconNonSelected={<OnAirIcon className="h-6 w-6 " />}
-        displayName="실시간"
-      />
-      <TabBarButton
-        title="recommend"
-        iconSelected={<RecommIconAbled className="h-6 w-6" />}
-        iconNonSelected={<RecommIcon className="h-6 w-6 " />}
-        displayName="뻔맵추천"
-      />
-      <TabBarButton
-        title="my"
-        iconSelected={<MyIconAbled className="h-6 w-6 " />}
-        iconNonSelected={<MyIcon className="h-6 w-6 " />}
-        displayName="MY"
-      />
-    </div>
-  );
+    return (
+        <div className="flex flex-row justify-evenly items-center shadow-md z-20 fixed bottom-0 pb-6 w-full max-w-[450px] bg-white pt-1 h-[68px]">
+            <TabBarButton
+                title="home"
+                iconSelected={<HomeIconAbled className="h-6 w-6" />}
+                iconNonSelected={<HomeIcon className="h-6 w-6" />}
+                displayName="홈"
+            />
+            <TabBarButton
+                title="onAir"
+                iconSelected={<OnAirIconAbled className="h-6 w-6 " />}
+                iconNonSelected={<OnAirIcon className="h-6 w-6 " />}
+                displayName="실시간"
+            />
+            <TabBarButton
+                title="recommend"
+                iconSelected={<RecommIconAbled className="h-6 w-6" />}
+                iconNonSelected={<RecommIcon className="h-6 w-6 " />}
+                displayName="뻔맵추천"
+            />
+            <TabBarButton
+                title="my"
+                iconSelected={<MyIconAbled className="h-6 w-6 " />}
+                iconNonSelected={<MyIcon className="h-6 w-6 " />}
+                displayName="MY"
+            />
+        </div>
+    );
 }
