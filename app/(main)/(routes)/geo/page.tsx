1ceafe66773010f1e16e0896d435cd76ae743f7b
dgfh0450 <dgfh0450@gmail.com>
2024-11-04 01:35:38 +0900

    #10-chore-geolocationAPI : Apply useWatchPosition hook

--- app/(main)/(routes)/geo/page.tsx
+++ app/(main)/(routes)/geo/page.tsx
@@ -1,24 +1,31 @@
 "use client";
 
-import { useGeoLocation } from "@/hooks/useGeoLocations";
+import { useGeoLocation, useWatchPosition } from "@/hooks/useGeoLocations";
 
 const geolocationOptions = {
-  enableHighAccuracy: true,
-  timeout: 1000 * 10,
-  maximumAge: 1000 * 3600 * 24,
+    enableHighAccuracy: true,
+    timeout: 1000 * 10,
+    maximumAge: 1000 * 3600 * 24,
 };
 
 const Weather = () => {
-  const { location, error } = useGeoLocation(geolocationOptions);
+    const { location, error } = useGeoLocation(geolocationOptions);
+    const { location: watch, error: watchError } = useWatchPosition(geolocationOptions);
 
-  return (
-    <div>
-      <h1>Weather</h1>
-      <p>Latitude: {location?.latitude}</p>
-      <p>Longitude: {location?.longitude}</p>
-      <p>Error: {error}</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Weather</h1>
+            <p>Latitude: {location?.latitude}</p>
+            <p>Longitude: {location?.longitude}</p>
+            <p>Error: {error}</p>
+
+
+            <h1>Weather</h1>
+            <p>Latitude: {watch?.latitude}</p>
+            <p>Longitude: {watch?.longitude}</p>
+            <p>Error: {error}</p>
+        </div>
+    );
 };
 
 export default Weather;
