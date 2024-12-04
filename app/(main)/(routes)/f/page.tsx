4a538fee448f78359634809ac9918ccf04b34a6f
dgfh0450 <dgfh0450@gmail.com>
2024-12-04 20:38:58 +0900

    #27 feat : Merge Error fix

--- app/(main)/(routes)/f/page.tsx
+++ app/(main)/(routes)/f/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import KakaoMap, {
-  BuildingFacilityInfo,
-  LatLng,
+    BuildingFacilityInfo,
+    LatLng,
 } from "@/app/_components/kakao-map";
 import { useSearchParams } from "next/navigation";
 import TopSearchResult from "../../_components/search-modal/top-search-result";
@@ -15,97 +15,97 @@ import { useSearchKeywordConvi } from "@/hooks/useSearchKeywordConvi";
 import { toast } from "sonner";
 
 type BuildingNum = {
-  buildingName: string;
-  longitude: number;
-  latitude: number;
-  num: number;
+    buildingName: string;
+    longitude: number;
+    latitude: number;
+    num: number;
 };
 
 type BuildingType = {
-  buildingName: string;
-  type: string;
-  longitude: number;
-  latitude: number;
+    buildingName: string;
+    type: string;
+    longitude: number;
+    latitude: number;
 };
 
 type ResponseData = BuildingNum | BuildingType;
 
 const fetchBuildingNums = async (facName: string, entity: boolean) => {
-  const url = entity
-    ? `${process.env.NEXT_PUBLIC_API_SERVER_MAIN_URL}/facility/${facName}`
-    : `${process.env.NEXT_PUBLIC_API_SERVER_MAIN_URL}/facilityLoc?type=${facName}`;
-  console.log(url);
-  const response = await fetch(url);
-  return response.json();
+    const url = entity
+        ? `${process.env.NEXT_PUBLIC_API_SERVER_MAIN_URL}/facility/${facName}`
+        : `${process.env.NEXT_PUBLIC_API_SERVER_MAIN_URL}/facilityLoc?type=${facName}`;
+    console.log(url);
+    const response = await fetch(url);
+    return response.json();
 };
 
 const Facility = () => {
-  const router = useRouter();
-  const params = useSearchParams();
-  const type = params.get("type") || "lounge";
-  const entity = params.get("entity") === "true";
-  const isConvi = params.get("convi") === "true";
-  const isConviFromSearchModal = params.get("isConvi") === "true";
-  const { setSearchKeywordConviTrue, isSearchKeywordConvi } =
-    useSearchKeywordConvi();
-  const {
-    setSearchBottomModalOpen,
-    isSearchBottomModalOpen,
-    bottomModalSearchBuilding,
-    setSearchBottomModalClose,
-  } = useSearchBottomModal();
+    const router = useRouter();
+    const params = useSearchParams();
+    const type = params.get("type") || "lounge";
+    const entity = params.get("entity") === "true";
+    const isConvi = params.get("convi") === "true";
+    const isConviFromSearchModal = params.get("isConvi") === "true";
+    const { setSearchKeywordConviTrue, isSearchKeywordConvi } =
+        useSearchKeywordConvi();
+    const {
+        setSearchBottomModalOpen,
+        isSearchBottomModalOpen,
+        bottomModalSearchBuilding,
+        setSearchBottomModalClose,
+    } = useSearchBottomModal();
 
-  const {
-    isPending: facilityIsPending,
-    error: facilityError,
-    data: facilityData,
-  } = useQuery<ResponseData[]>({
-    queryKey: ["facilityNumList", type],
-    queryFn: () => fetchBuildingNums(type, entity),
-  });
-  useEffect(() => {
-    // isConviFromSearchModal이 true일 경우 setSearchKeywordConviTrue 실행
-    if (isConviFromSearchModal) {
-      setSearchKeywordConviTrue();
+    const {
+        isPending: facilityIsPending,
+        error: facilityError,
+        data: facilityData,
+    } = useQuery<ResponseData[]>({
+        queryKey: ["facilityNumList", type],
+        queryFn: () => fetchBuildingNums(type, entity),
+    });
+    useEffect(() => {
+        // isConviFromSearchModal이 true일 경우 setSearchKeywordConviTrue 실행
+        if (isConviFromSearchModal) {
+            setSearchKeywordConviTrue();
+        }
+        setSearchBottomModalClose();
+        console.log(facilityData);
+        // 기타 로그 출력
+        console.log("isConvi", isConvi);
+        console.log("isSearchKeywordConvi", isSearchKeywordConvi);
+        console.log("isConviFromSearchModal", isConviFromSearchModal);
+    }, [isConvi, isConviFromSearchModal, isSearchKeywordConvi]);
+    if (facilityData && facilityData?.length === 1) {
+        router.push(`/b?type=${type}&building=${facilityData[0].buildingName}`);
     }
-    setSearchBottomModalClose();
-    console.log(facilityData);
-    // 기타 로그 출력
-    console.log("isConvi", isConvi);
-    console.log("isSearchKeywordConvi", isSearchKeywordConvi);
-    console.log("isConviFromSearchModal", isConviFromSearchModal);
-  }, [isConvi, isConviFromSearchModal, isSearchKeywordConvi]);
-  if (facilityData && facilityData?.length === 1) {
-    router.push(`/b?type=${type}&building=${facilityData[0].buildingName}`);
-  }
 
-  const facilityInfos: BuildingFacilityInfo[] | undefined = Array.isArray(
-    facilityData
-  )
-    ? facilityData.map((item) => ({
-        lat: item.latitude,
-        lon: item.longitude,
-        name: item.buildingName,
-      }))
-    : undefined;
+    const facilityInfos: BuildingFacilityInfo[] | undefined = Array.isArray(
+        facilityData
+    )
+        ? facilityData.map((item) => ({
+            lat: item.latitude,
+            lon: item.longitude,
+            name: item.buildingName,
+        }))
+        : undefined;
 
-  return (
-    <div className="w-full max-w-[450px] h-full left-0 top-0">
-      <TopSearchResult searchKeyword={type} />
-      <KakaoMap
-        markers={facilityInfos}
-        center={{ lat: 37.58706787265499, lon: 127.02916578491909 }}
-        markerModalEvent={true}
-      />
-      {isSearchBottomModalOpen && (
-        <SearchBottomModal
-          searchType={type!}
-          searchContext={true}
-          searchText={type}
-        />
-      )}
-    </div>
-  );
+    return (
+        <div className="w-full max-w-[450px] h-full left-0 top-0">
+            <TopSearchResult searchKeyword={type} />
+            <KakaoMap
+                markers={facilityInfos}
+                // center={{ lat: 37.58706787265499, lon: 127.02916578491909 }}
+                markerModalEvent={true}
+            />
+            {isSearchBottomModalOpen && (
+                <SearchBottomModal
+                    searchType={type!}
+                    searchContext={true}
+                    searchText={type}
+                />
+            )}
+        </div>
+    );
 };
 
 export default Facility;
