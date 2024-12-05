e515929c81aac739607b4cd3f14db9b2352ec84d
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2024-12-05 20:39:56 +0900

    #27 feat : OnAirResult redirect to home & loading Skeleton (#31)
    * #27 feat : OnAirResult redirect to home & loading Skeleton

--- app/(main)/(routes)/onAir/detail/page.tsx
+++ app/(main)/(routes)/onAir/detail/page.tsx
@@ -16,10 +16,11 @@ import Arrow from '@/public/Arrow.svg';
 import Star from '@/public/onAir/Star_fill.svg';
 import StarNone from '@/public/onAir/Star_none.svg';
 import Link from 'next/link';
-import { detailCategoryMap, fetchLike, getOnAirPlace, getPlaceDetail } from '../fetch';
+import { BuildingTypeMap, detailCategoryMap, fetchLike, getOnAirPlace, getPlaceDetail } from '../fetch';
 import { useSession } from 'next-auth/react';
 import LoginError from '../CustomError';
 import CarouselIndicator from '@/components/ui/carousel-indicator';
+import { Skeleton } from '@/components/ui/skeleton';
 
 
 
@@ -55,7 +56,8 @@ export default function OnAirResultDetail() {
             else return false;
         }
     })
-    if (statusPlace == 'pending' || statusDetail == 'pending') return <div>loading...</div>
+    if (statusPlace == 'error' || statusDetail == 'error') return <div>error</div>
+    if (statusPlace == 'pending' || statusDetail == 'pending') return <DetailSkeleton />
     if (!queryPlace || !place || !detail || error) return <div>no place</div>
 
     const { buildingName, floor, placeName, placeType, vote, result, like, voteAvailable } = place;
@@ -104,7 +106,7 @@ export default function OnAirResultDetail() {
             <div className='px-5 py-[18px]'>
                 <div className='flex flex-row justify-between'>
                     <p className='font-semibold text-lg'>상세정보</p>
-                    <Link href={'/'} className='flex flex-row items-center font-semibold text-[13px]'>공간 위치보기 <Arrow className='ml-2' stroke='#000000' width={6.4} height={12} /></Link>
+                    <Link href={BuildingTypeMap.has(placeType) ? `/b?type=${BuildingTypeMap.get(placeType)}&building=${buildingName}&fromOnAir=true` : `/`} className='flex flex-row items-center font-semibold text-[13px]'>공간 위치보기 <Arrow className='ml-2' stroke='#000000' width={6.4} height={12} /></Link>
                 </div>
                 <div className='w-[full]'>
                     {
@@ -126,6 +128,49 @@ export default function OnAirResultDetail() {
                             d => detailCategoryMap.has(d)).map(
                                 (el, idx) => <p key={`${placeName}-detail-category-${idx}`} className='text-sm mb-[11px]'><span className='inline-block text-gray-500 w-[55px] mr-1 font-medium'>{detailCategoryMap.get(el)}</span> <span className='text-sm font-regular'>{detail[el]}</span></p>
                             )
+                    }
+                </div>
+            </div>
+        </div >
+    )
+}
+
+const DetailSkeleton = () => {
+    return (
+        <div className='w-full max-w-[450px] h-full py-[51px] relative flex flex-col justify-start'>
+            <div className='mx-5'>
+                <div className='flex flex-row justify-between mb-5'>
+                    <Link href={'/onAir/result'}><Arrow stroke='#000000' width={12} height={22.5} strokeWidth={0.6} className=' rotate-180 translate-x-[20%]' /></Link>
+                    <StarNone />
+                </div>
+                <Skeleton className='w-[70%] h-[30px] mb-1' />
+                <Skeleton className='w-[30%] h-[18px]' />
+            </div>
+            <div className='relative my-16 mx-5'>
+                <div className='mb-[37px] h-[7px] w-full' />
+                <div className={`${styles.gradient_bar} w-full h-[15px] border border-[#E4E4E4] rounded-full mt-2 mb-2`} />
+                <div className='flex justify-between'>
+                    <span className='text-[12px] font-regulars ml-[3px] text-gray-500'>다 차 있어요</span>
+                    <span className='text-[12px] font-regulars mr-[3px] text-gray-500'>빈 자리 많아요</span>
+                </div>
+            </div>
+            <Link href={'/onAir'} className='w-auto border border-gray-100 mx-5 my-8 px-4 py-[11px] rounded-[5px] text-sm flex flex-row justify-between items-center'>
+                <span><span className='text-point font-bold'>실시간 정보</span>는 어떻게 수집되나요</span>
+                <Arrow stroke='rgb(209 213 219)' className='' width={8} height={15} />
+            </Link>
+            <div className='w-full h-[8px] bg-gray-50' />
+            <div className='px-5 py-[18px]'>
+                <div className='flex flex-row justify-between'>
+                    <p className='font-semibold text-lg'>상세정보</p>
+                </div>
+                <div className='w-[full]'>
+                    <Skeleton className='mx-12 my-6 rounded-[10px] h-[180px]' />
+                </div>
+                <div className='ml-1'>
+                    {
+                        Array(3).fill(0).map(
+                            (el, idx) => <Skeleton key={`Skeleton-detail-category-${idx}`} className='h-[21px] mb-[11px]'></Skeleton>
+                        )
                     }
                 </div>
             </div>
